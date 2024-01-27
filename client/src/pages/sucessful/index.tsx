b957cade4ecce31d57f767cab42786cffc9e1ec5
kiritoEM <johankirito64@gmail.com>
2024-01-27 08:33:28 +0100

    add redirect-URL

--- client/src/pages/sucessful/index.tsx
+++ client/src/pages/sucessful/index.tsx
@@ -1,10 +1,26 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 const index = (): JSX.Element => {
+  const router = useRouter();
   return (
-    <section id="accueil">
-      <h1>
-        Authentification 0Auth <span>réussie ! </span>
-      </h1>
-    </section>
+    <Fragment>
+      <Head>
+        <title>0Auth - réussite</title>
+      </Head>
+      <section id="accueil">
+        <div className="title">
+          <h1>
+            Authentification 0Auth <span>réussie ! </span>
+          </h1>
+        </div>
+
+        <div className="button" onClick={() => router.push("/")}>
+          <button className="btn">Revenir au login</button>
+        </div>
+      </section>
+    </Fragment>
   );
 };
 
