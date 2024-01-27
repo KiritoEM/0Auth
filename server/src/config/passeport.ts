30971a5bf05cab287d0fb3191f698e27007c11a8
kiritoEM <johankirito64@gmail.com>
2024-01-27 18:58:20 +0100

    add passeport-authFacebook config

--- server/src/config/passeport.ts
+++ server/src/config/passeport.ts
@@ -1,9 +1,14 @@
 // passport.ts
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import {
+  Strategy as FacebookStrategy,
+  Profile as FacebookProfile,
+} from "passport-facebook";
 import dotenv from "dotenv";
 dotenv.config();
 
+//passeport Google
 passport.use(
   new GoogleStrategy(
     {
@@ -15,9 +20,33 @@ passport.use(
         "https://www.googleapis.com/auth/userinfo.email",
       ],
     },
-    (accessToken, refreshToken, profile, done) => {
+    (_accessToken, _refreshToken, profile, done) => {
       console.log("profile de l' utilisateur", profile);
       return done(null, profile);
+    }
+  )
+);
+
+//passeport Facebook
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: `http://localhost:${process.env.PORT}/api/auth/facebook/callback`,
+      profileFields: ["id", "displayName", "email"],
+    },
+    (_accessToken, _refreshToken, profile: FacebookProfile, done) => {
+      try {
+        console.log("Profile de l'utilisateur Facebook", profile);
+        return done(null, profile);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la gestion de l'authentification Facebook",
+          error
+        );
+        return done(error, null);
+      }
     }
   )
 );
