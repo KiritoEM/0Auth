e4730c3ce719060efaf27cdecd0aca670f54e947
kiritoEM <johankirito64@gmail.com>
2024-01-27 03:49:35 +0100

    backend add passeport config

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,20 +1,27 @@
 import ButtonsLogin from "@/components/ButtonsLogin";
 import Header from "@/components/Header";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const login = (): JSX.Element => {
   return (
-    <section id="login">
-      <div id="login__container">
-        <div className="shape-1">
-          <img src="/shape-1.png" alt="" />
+    <Fragment>
+      <Head>
+        <title>0Auth</title>
+      </Head>
+      <section id="login">
+        <div id="login__container">
+          <div className="shape-1">
+            <img src="/shape-1.png" alt="" />
+          </div>
+          <Header />
+          <ButtonsLogin />
+          <div className="shape-2">
+            <img src="/shape-2.png" alt="" />
+          </div>
         </div>
-        <Header />
-        <ButtonsLogin />
-        <div className="shape-2">
-          <img src="/shape-2.png" alt="" />
-        </div>
-      </div>
-    </section>
+      </section>
+    </Fragment>
   );
 };
 
